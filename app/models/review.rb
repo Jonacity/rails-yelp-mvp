1aaa65980de4e27b225a8ec8265b6e6a2706e732
jonathan sitti <sitti.jonathan@gmail.com>
2018-06-06 12:48:48 +0200

    models done and controllers created

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
 
   validates :restaurant_id, presence: true
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
   validates_type :rating, :integer
 end
